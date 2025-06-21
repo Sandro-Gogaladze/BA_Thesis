948a955d6ec9c0a7f36021d50f95af81a7f8e4b4
Sandro-Gogaladze <sandro.gogaladze@iset.ge>
2025-06-21 23:47:01 +0400

    Ensured reproducability - fixed pyproject.toml

---
+++ IncomeEstimation/__init__.py
@@ -0,0 +1,7 @@
+"""Income Estimation Models Analysis Package.
+
+This package contains models and utilities for income estimation analysis,
+focusing on balancing accuracy with regulatory requirements in the Georgian Banking Sector.
+"""
+
+__version__ = "1.0.0"
